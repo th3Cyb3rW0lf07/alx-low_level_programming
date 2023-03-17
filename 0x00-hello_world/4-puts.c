b3df5d50c8ae8850cf811bae16871b606a067a71
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-16 19:38:36 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
-
-int main ()
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	put("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
