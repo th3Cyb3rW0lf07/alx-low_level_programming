2d7223f73288214c97d0d55460ca43acc05bb9c4
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-21 08:17:51 -0700

    Task 1, printing all alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all letters of the alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char l;
+	
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+		_putchar('\n');
+}
