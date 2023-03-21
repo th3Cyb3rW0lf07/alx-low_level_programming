227b8eeca9ffcb927ec758245dcea65abbd71615
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-21 12:10:41 -0700

    Task 6, computing the absolute value of integers, adjusted the description in 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
  * _abs - computes the absolute value of an integer
+ * @n: the integer to be computed
+ *
+ * Return: the absolute value of the integer
  */
 
 int _abs(int n)
