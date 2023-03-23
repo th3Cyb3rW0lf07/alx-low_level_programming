2c45dbadabed34af3ed1f9fc2614735fbf50438c
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-23 16:59:51 -0700

    Task 7, prints a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: the number of lines the diagonal will be
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	int h;
+
+	for (h = 0; h < n; h++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
