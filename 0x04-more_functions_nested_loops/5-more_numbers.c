755e8f927987fa5c42465254e9d6cfa45e5e1a06
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-23 16:28:33 -0700

    Task 5, corrected issues

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 0 to 14, 10 times followed by a new line
- * Return: doesn't return anything
+ * more_numbers - prints numbers between 0 to 14 10 times.
+ * Return: no return.
  */
-
 void more_numbers(void)
 {
-	int i, h;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (h = 0; h < 15; h++)
+		for (j = 0; j < 15; j++)
 		{
-			if (h >= 10)
-				_putchar((h / 10) + 48);
-				_putchar((h % 10) + 48);
+			if (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
