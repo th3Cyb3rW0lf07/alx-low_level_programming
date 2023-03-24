10e59a8eecf5a2385757d1644509529a2ad993a4
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-23 17:07:16 -0700

    Task 7, it works now

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints a diagonal line
- * @n: the number of lines the diagonal will be
- * Return: nothing
+ * print_diagonal - prints diagonal line n times.
+ * @n: times diagonal line is printed.
+ * Return: nothing.
  */
-
 void print_diagonal(int n)
 {
-	int h;
+	int i, g;
 
-	for (h = 0; h < n; h++)
+	for (i = 0; i < n; i++)
 	{
+		for (g = 0; g < i; g++)
+		{
+			_putchar(' ');
+		}
 		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
