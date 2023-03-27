11d398bfdc5a9f637a681531bf251811ff8bffdd
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-27 06:23:52 -0700

    Task 0, sets a pointer to a parameter and updates the value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer as a parameter and updates the value.
+ * @n: the input integer
+ * Return: Doesn't return anything
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
