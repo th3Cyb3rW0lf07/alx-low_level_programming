ce0610512bc793eac15216c44c4cb29a533b4f3f
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-31 10:53:35 +0100

    Task 4, reverses an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @n: number of integers in the array
+ * @a: the array
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, t;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		t = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = t;
+	}
+}
