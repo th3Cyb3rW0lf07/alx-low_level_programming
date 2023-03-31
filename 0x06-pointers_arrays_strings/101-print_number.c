0cf5341ea8a8d241fb5f264dcf4832be3f68614b
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-03-31 12:39:41 +0100

    Task 9, prints out integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_number - Prints any integer with putchar
+  * @n: Number to print
+  * Return: Nothing
+  */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
