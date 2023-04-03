1745f081246137e954fc757af8100ef0805e1470
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-04-03 15:30:39 +0100

    Task 6, prints out an 8x8 chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints an 8x8 chessboard
+ * @a: pointer for the peices
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int k, l;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (l = 0; l < 8; l++)
+		{
+			_putchar(a[k][l]);
+		}
+		_putchar("\n");
+	}
+}
