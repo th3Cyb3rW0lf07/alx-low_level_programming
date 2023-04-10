ebad33d91a219d861260230792d32e19322bc6d0
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-04-10 21:43:56 +0100

    Task 1, prints number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: the number of arguments
+ * Return: 0 if succesful
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
