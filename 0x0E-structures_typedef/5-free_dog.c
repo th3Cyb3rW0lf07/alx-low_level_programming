d781554793958bb7fb4b0821c9f74ef578219663
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-04-20 13:35:02 +0100

    Task 5, added 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees all dogs
+ * Return: nothing
+*/
+
+void free_dog(dog_t *d)
+{
+    if (d)
+    {
+        free(d->name);
+        free(d->owner);
+        free(d);
+    }
+}
