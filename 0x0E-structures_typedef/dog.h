1afde9a0904d5845fc9d26b603d54df58df1ce26
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-04-20 12:12:45 +0100

    Added README.md and dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that stores a dog's info
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: a struct called dog that stores its name,
+ * the age and the name of its owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
