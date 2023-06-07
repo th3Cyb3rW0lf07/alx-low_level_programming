31367c236c3731bc414d115186077598b969658d
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-07 16:03:01 +0100

    Task 8, done

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * wildcmp - function that compares 2 strings to see if they can be
+ * identical
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 1 if they're identical, 0 if otherwise
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1))
+		{
+			return (1);
+		}
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+		{
+			return (1);
+		}
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
