b290ac681a382e873e8db4547e7c30e09d3885f2
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-09 23:52:55 +0100

    Task 0, done Added README.md

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program, foolowed by a new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: the program name
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+}
