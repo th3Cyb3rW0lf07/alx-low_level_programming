9a0890b74eacfce6d8ddf1356018db7921ea01f0
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-10 01:17:30 +0100

    Final task done Added 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - calculates the amount of change and number of coins to be given
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 if successful, 1 if otherwise
+*/
+
+int main(int argc, char *argv[])
+{
+	int cents, coins;
+	int coin25 = 25, coin10 = 10, coin5 = 5, coin2 = 2, coin1 = 1;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (cents > 0)
+	{
+		switch (cents)
+		{
+			case 25:
+				cents -= coin25;
+				break;
+			case 10:
+				cents -= coin10;
+				break;
+			case 5:
+				cents -= coin5;
+				break;
+			case 2:
+				cents -= coin2;
+				break;
+			default:
+				cents -= coin1;
+				break;
+		}
+		coins++;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
