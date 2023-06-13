763a560ab3e0e3b4db26f51decd09d08734e7283
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-13 18:33:26 +0100

    Task 1, done Added 1-strdup.cand modified main.h

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter
+ * @str: the string to be copied
+ * Return: a pointer to the newly allocated space
+*/
+
+char *_strdup(char *str)
+{
+	size_t i, len;
+	char *dupl;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	dupl = (char *)malloc((len + 1) * sizeof(char));
+
+	if (dupl == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		dupl[i] = str[i];
+	}
+
+	return (dupl);
+}
