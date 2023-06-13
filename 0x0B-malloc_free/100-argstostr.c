bbe635b0e07f3a32aa31eba564e2a37e8f61f33f
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-13 22:27:26 +0100

    Advanced task 1, done Added 100-argstostr.c and mod main.h

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all args of a program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: the pointer to the new string
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, length, curr_index = 0;
+	char *concat_str;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+		length++;
+	}
+
+	concat_str = malloc((length + 1) * sizeof(char));
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			concat_str[curr_index++] = av[i][j];
+		}
+		concat_str[curr_index++] = '\n';
+	}
+	concat_str[curr_index] = '\0';
+
+	return (concat_str);
+
+}
