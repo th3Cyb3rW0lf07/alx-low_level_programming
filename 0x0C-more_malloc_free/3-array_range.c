f0beb81e11eb127356c97cc80ad63dc9b88a47e1
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-19 16:27:26 +0100

    Betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,20 +11,20 @@
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int size, i;
+	int *arr;
+	int size, i;
 
-    if (min > max)
-        return (NULL);
+	if (min > max)
+		return (NULL);
 
-    size = max - min + 1;
-    arr = malloc(sizeof(int) * size);
+	size = max - min + 1;
+	arr = malloc(sizeof(int) * size);
 
-    if (arr == NULL)
-        return (NULL);
+	if (arr == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = min++;
+	for (i = 0; i < size; i++)
+		arr[i] = min++;
 
-    return (arr);
+	return (arr);
 }
