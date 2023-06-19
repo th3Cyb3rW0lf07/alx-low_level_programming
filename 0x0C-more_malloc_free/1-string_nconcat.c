0d6f9af8e60756054cc5af2667c15d5aa9dae0e4
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-19 15:57:53 +0100

    Betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	concatenated = malloc(sizeof(char) * (l1 + n + 1));
 
 	if (concatenated == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < l1; i++)
 		concatenated[i] = s1[i];
