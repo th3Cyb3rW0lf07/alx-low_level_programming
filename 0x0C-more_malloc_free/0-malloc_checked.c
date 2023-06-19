64db9a572aede6cf157b54d2fc3bdcdf5e767e33
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-19 15:21:28 +0100

    Betty style

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Size of the memory to allocate
+ * Return: Pointer to the allocated memory
+ *
+ * If malloc fails, the function terminates the process with a status value of 98
+ */
+
 void *malloc_checked(unsigned int b) {
 	void *p = malloc(b);
 	if (p == NULL) {
