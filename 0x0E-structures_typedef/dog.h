87ff7721c4b43b8e3fc27b717b9100df421e2574
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-20 08:49:46 +0100

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,20 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog - Struct for dog
+ * @name: pointer to the name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ * dog_t - typedef for struct dog
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
