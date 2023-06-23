a9672310f6b5c201b8a6f7784d295fb6d3171f70
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-23 03:42:31 +0100

    Task 2, done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int
+ * @array: the array to be searched
+ * @size: the size of the array
+ * @cmp: the function pointer
+ * Return: the value of the index
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
