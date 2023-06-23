cb66ab34ae410f9c9c295a4d9c1df076a7ecd81b
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-23 03:28:00 +0100

    Task 1, done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through an array
+ * @array: the array
+ * @size: the size of the array
+ * @action: the function pointer
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
