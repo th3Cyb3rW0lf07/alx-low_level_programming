25d3b1c1d52d525fb21cbc1074ed45746f8fe0d3
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-06-24 16:58:29 +0100

    Task 3, completed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -29,11 +29,7 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				str = va_arg(args, char *);
-
-				if (str == NULL)
-					printf("(nil)");
-				else
-					printf("%s", str);
+				printf("%s", str ? str : "(nil)");
 				break;
 			default:
 				break;
