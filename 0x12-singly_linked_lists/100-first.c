fb59bf07402cb5fbf2fcca14c509ff1008d5e800
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-07-03 16:29:35 +0100

    Advnaced task 1, corrected and completed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,21 @@
-#include "lists.h"
+#include <stdio.h>
+
+void my_const() __attribute__((constructor));
 
 /**
  * print - prints a message
 */
 
-void print()
+void print(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	"I bore my house upon my back!\n");
 }
 
-void __attribute__((constructor)) print(void);
+/**
+ * my_const - Ensures print is executed before main
+*/
+void my_const(void)
+{
+	print();
+}
