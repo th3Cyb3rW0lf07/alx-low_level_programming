91127b139a5a70a88f7464421c19270654b09a00
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-07-03 14:22:05 +0100

    Made final changes to task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,20 +11,16 @@ size_t print_list(const list_t *h)
 	const list_t *current = h;
 	size_t nodeCount = 0;
 
-	printf("[");
 	while (current != NULL)
 	{
 		if (current->str != NULL)
-			printf("%s", current->str);
+			printf("[%u] %s\n", current->len, current->str);
 		else
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 
 		current = current->next;
-		if (current != NULL)
-			printf(", ");
 		nodeCount++;
 	}
 
-	printf("]\n");
 	return (nodeCount);
 }
