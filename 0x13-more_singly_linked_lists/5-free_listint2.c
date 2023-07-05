32fdc3dded460663eb5b8a83a1c4c5aa764e2876
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-07-05 12:39:19 +0100

    Task 5, done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the list and also sets head to NULL
+ * @head: pointer to the head of the list
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr;
+
+	while (*head != NULL)
+	{
+		curr = *head;
+		*head = (*head)->next;
+		free(curr);
+	}
+}
