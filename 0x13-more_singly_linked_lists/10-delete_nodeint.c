f3ca5f34cd2680603fdf2334ed8e4d7a1229d6d9
th3Cyb3rW0lf07 <th3cyb3rw0lf@proton.me>
2023-07-05 22:28:07 +0100

    Betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	current = *head;
-	while (current != NULL && curr_index < index -1)
+	while (current != NULL && curr_index < index - 1)
 	{
 		current = current->next;
 		curr_index++;
